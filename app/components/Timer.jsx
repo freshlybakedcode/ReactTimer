3baa2ffa9f58521913b7aec4ac5e6eea61df5eec
James Brenton <freshlybakedcode@jamesbrenton.co.uk>
2016-08-22 19:31:49 +0100

    Added timer and countdown modules and set up nav

---
+++ app/components/Timer.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var Timer = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <p>This is timer.jsx</p>
+      </div>
+    );
+  }
+})
+
+module.exports = Timer;
