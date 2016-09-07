a748c19484b673be346f655b2154b41fda10b173
James Brenton <freshlybakedcode@jamesbrenton.co.uk>
2016-09-07 17:44:24 +0100

    Added coundown form component and tests

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -5,8 +5,8 @@ var Main = (props) => {
   return (
     <div>
       <Navigation />
-      <div className = "row">
-        <div className = "small-centered medium-6 large-4 columns">
+      <div className="row">
+        <div className="small-centered medium-6 large-4 columns">
           <h1 className="text-center">React timer</h1>
           {props.children}
         </div>
